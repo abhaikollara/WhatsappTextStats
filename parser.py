345e5beb33cec98415491e804ce33400bb16ab36
Abhai Kollara <abhai.dilip@gmail.com>
2020-03-07 22:35:35 +0530

    Remove old code

--- parser.py
+++ parser.py
@@ -11,22 +11,6 @@ def _is_next_message(raw_message):
     return True
 
 
-# def parse_file(path):
-#     with open(path) as f:
-#         raw_data = f.readlines()
-
-#     name = path.split('with ')[1].split('.txt')[0]
-#     conversation = Conversation(name)
-
-#     buffer = raw_data[0]
-#     for line in raw_data[1:]:
-#         if _is_next_message(line):
-#             conversation.add_raw_message(buffer.rstrip())
-#             buffer = line
-#         else:
-#             buffer += line
-
-
 def parse_file(f):
     raw_data = [str(x, encoding="utf-8") for x in f.readlines()]
     name = f.filename.split("with ")[1].split(".txt")[0]
